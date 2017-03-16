748dba5574f07d3651a5a495f44ea5a2bc60b317
jamie <jmleary@willamette.edu>
2017-03-15 20:44:27 -0700

    Added QuickSort

--- src/algorithms2/QuickSort.java
+++ src/algorithms2/QuickSort.java
@@ -15,10 +15,30 @@ class QuickSort {
         quicksort(nums,0,nums.length-1);
     }
     
-    void quicksort(int[] nums, int p, int q) {
+    void quicksort(int[] nums, int p, int r) {
         if (p < r) {
-            q = 
+            int q = partition(nums,p,r);
+            quicksort(nums,p,q-1);
+            quicksort(nums,q+1,r);
         }
+    }
+    
+    int partition(int[] nums, int p, int r) {
+        int x = nums[r];
+        int i = p-1;
+        int temp;
+        for (int j = p; j < r; j++) {
+            if (nums[j] <= x) {
+                i++;
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        temp = nums[i+1];
+        nums[i+1] = nums[r];
+        nums[r] = temp;
+        return i+1;
     }
     
 }
